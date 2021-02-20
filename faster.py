a5cb0591d557478671a7872b537c31c944411724
thangnch <thangnch@gmail.com>
2021-02-20 11:30:25 +0700

    Add files via upload

--- faster.py
+++ faster.py
@@ -11,24 +11,25 @@ print("Process time = ", time.time()-start)
 print("Load from file to data frame ")
 start = time.time()
 df = vaex.open(file_path + ".hdf5")
+print(df['column_1'].sum())
 print("Process time = ", time.time()-start)
-#
-# print("Show first 10 record")
-# print(df.head(10))
 
 print("Try to multiply some columns")
 start = time.time()
 df['column_test']=df.column_1 * df.column_3
+print(df['column_test'].sum())
 print("Process time = ", time.time()-start)
 
 print("Try to filter by value")
 start = time.time()
 df = df[df['column_13']>0]
+print(df['column_test'].sum())
 print("Process time = ", time.time()-start)
 
 print("Try to group by column")
 start = time.time()
 df = df.groupby(df.column_test, agg=vaex.agg.mean(df.column_1))
+print(df['column_test'].sum())
 print("Process time = ", time.time()-start)
 
 df = None
--- main.py
+++ main.py
@@ -25,21 +25,25 @@ print("Process time = ", time.time()-start)
 print("Load from file to data frame ")
 start = time.time()
 df = pd.read_csv(file_path)
+(df.head())
 print("Process time = ", time.time()-start)
 
 print("Try to multiply some columns")
 start = time.time()
 df['column_test']=df.column_1 * df.column_3
+(df.head())
 print("Process time = ", time.time()-start)
 
 print("Try to filter by value")
 start = time.time()
 df = df[df['column_13']>0]
+(df.head())
 print("Process time = ", time.time()-start)
 
 print("Try to group by column")
 start = time.time()
 df = df.groupby(by="column_13").mean()
+(df.head())
 print("Process time = ", time.time()-start)
 
 df = None
